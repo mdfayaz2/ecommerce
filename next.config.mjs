e7d812a119c3e3798262c2a6604a5ddfe6478da3
mdfayaz2 <kmdfayaz@lyrostech.com>
2024-07-16 21:31:51 +0530

    final commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+  };
+  
+  export default nextConfig;
+  
